9b5342fad10d830db200aedad4b3013ea4b5ceb8
Alex Polyachinscky <polya4inscky.alex@yandex.ru>
2020-03-13 21:40:27 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let checkStr = str
+  let brackets = bracketsConfig.map(item=>item.reduce((acc, cur)=>acc+ cur))
+  for(let i = str.length; i>=0; i--){
+  brackets.forEach(item=> {
+  str = str.replace(item, '')
+  })
+  if (str.length === 0) return true 
+  }
+  return false
 }
