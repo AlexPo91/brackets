313fcfdbe3ed600fa62070464adedde0fca61fd7
Alex Polyachinscky <polya4inscky.alex@yandex.ru>
2021-02-03 11:31:34 +0300

    refactor: delete string

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-  let checkStr = str
-  let brackets = bracketsConfig.map(item=>item.reduce((acc, cur)=>acc+ cur))
-  for(let i = str.length; i>=0; i--){
-  brackets.forEach(item=> {
-  str = str.replace(item, '')
-  })
-  if (str.length === 0) return true 
+  let brackets = bracketsConfig.map(el=>el.reduce((a, b)=> a+b))
+  for(let i = str.length; i >=0; i--){
+      brackets.forEach(el=> {
+          str = str.replace(el, '')
+        })
+        if(str.length === 0) return true
   }
   return false
 }
